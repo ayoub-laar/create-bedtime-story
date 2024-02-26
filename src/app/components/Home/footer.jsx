850d2dfb0bc48042b1c81006ee4d875d718dccb7
Syn2ed <ayoub.laarobi@gmail.com>
2024-02-26 16:12:06 +0100

    Improve landing page

--- src/app/components/Home/footer.jsx
+++ src/app/components/Home/footer.jsx
@@ -1,6 +1,4 @@
-import { Divider } from "@nextui-org/divider";
-import { Button } from "@nextui-org/button";
-import { Github, Twitter } from "lucide-react";
+import { Divider } from "@nextui-org/divider"
 export default function Footer() {
   return (
     <footer className="pt-24">
@@ -8,23 +6,11 @@ export default function Footer() {
         <Divider />
         <div className="flex justify-between items-center py-10">
           <p className="font-light">
-            Made by{" "}
-            <a
-              href="https://twitter.com/gonzalochale"
-              target="_blank"
-              className="font-medium"
-            >
-              Gonzalo Chale
-            </a>{" "}
-            with ❤️
-          </p>
-          <a href="https://github.com/gonzalochale" target="_blank">
-            <Button isIconOnly variant="light">
-              <Github size={24} />
-            </Button>
-          </a>
+            KidsAdventure | 
+            Made with ❤️
+          </p>  
         </div>
       </div>
     </footer>
-  );
+  )
 }
