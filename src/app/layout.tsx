f2561b185df1dbac8435a73d074a498e0e273673
Syn2ed <ayoub.laarobi@gmail.com>
2024-02-28 10:19:51 +0100

    Improve server/client side rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
-'use client'
-
 import './globals.css'
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { Ubuntu } from 'next/font/google'
-import { NextUIProvider } from '@nextui-org/react'
-import { ThemeProvider as NextThemesProvider } from "next-themes"
 import NavBar from "./components/Home/NavBar"
 import Footer from "./components/Home/Footer"
+import Providers from './providers'
 
 const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
   subsets: ['latin']
 })
 
-// export const metadata: Metadata = {
-//   title: 'KidsAdventure: Magical tales',
-//   description: 'Give your child a night full of dreams with their favorite characters'
-// }
+export const metadata: Metadata = {
+  title: 'KidsAdventure: Magical tales',
+  description: 'Give your child a night full of dreams with their favorite characters'
+}
 
 export default function RootLayout({
   children,
@@ -26,15 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={ubuntu.className}>
-        <NextUIProvider>
-          <NextThemesProvider attribute="class" defaultTheme="dark">
-            <div className="flex flex-col min-h-screen">
-              <NavBar />
-              {children}
-              <Footer />
-            </div>
-          </NextThemesProvider>
-        </NextUIProvider>
+        <Providers>
+          <div className="flex flex-col min-h-screen">
+            <NavBar />
+            {children}
+            <Footer />
+          </div>
+        </Providers>
       </body>
     </html>
   )
