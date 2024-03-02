91b06fc407ec955303ad97656a5965f591738283
Syn2ed <ayoub.laarobi@gmail.com>
2024-03-02 10:04:11 +0100

    Error msg when 0 characters choiced

--- src/app/components/Generate/FormStep/YourCharacters/index.tsx
+++ src/app/components/Generate/FormStep/YourCharacters/index.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import { Fragment } from "react"
+import { Fragment, useRef, useEffect } from "react"
 
 import { useFormStep } from "../../../../hooks/use-form-step"
 import { useLocalStorage } from "../../../../hooks/use-local-storage"
@@ -22,6 +20,9 @@ export function YourCharacters() {
 
   const images = ['/images/characters/mario_hd.png', '/images/characters/pikachu_hd.png', '/images/characters/spiderman.png', '/images/characters/superman.png', '/images/characters/homer.png', '/images/characters/naruto.png', '/images/characters/shrek.png', '/images/characters/mickey.png', '/images/characters/lion.png']
 
+  // Création d'une référence pour le message d'erreur
+  const errorMessageRef = useRef(null);
+
   function validateForm() {
     let formHasError = false
 
@@ -40,6 +41,11 @@ export function YourCharacters() {
         characters: charactersField
       }))
       handleNextStep()
+    } else {
+      // Si le formulaire n'est pas valide, effectue le scrolling vers le message d'erreur
+      if (errorMessageRef.current) {
+        errorMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
     }
   }
 
@@ -48,9 +54,12 @@ export function YourCharacters() {
       <Form.Card>
         <Form.Header
           title="SELECT CHARACTERS"
-          description="Let your child choice the characters is he prefer :)"
+          description="Let your child choose their favorite characters :)"
         />
         <div className="mt-5 flex flex-col gap-4">
+          {charactersField.hasError && (
+            <span ref={errorMessageRef} className="text-red-600 text-xl sm:text-sm">⚠️ {charactersField.errorMessage}</span>
+          )}
           <ImagesInput
             images={images}
             value={charactersField.value}
@@ -68,4 +77,4 @@ export function YourCharacters() {
       />
     </Fragment>
   )
-} 
+}
