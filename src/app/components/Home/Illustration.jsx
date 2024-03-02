3013602dbce7fc5284dec2b222c1a3b8e7a855e5
Syn2ed <ayoub.laarobi@gmail.com>
2024-03-02 07:59:06 +0100

    compress home image

--- src/app/components/Home/Illustration.jsx
+++ src/app/components/Home/Illustration.jsx
@@ -4,7 +4,7 @@ import { Image } from "@nextui-org/react"
 export default function Illustration() {
   return (
     <section className="relative max-w-screen-xl mx-auto md:px-8 flex flex-col justify-center items-center">
-        <Image isBlurred src="/images/illustration.png" width={500} height={500} />
+        <Image isBlurred src="/images/home_illustration.png" width={500} height={500} />
     </section>
   );
 }
