ce24c9803b96d27bd5e98ae1690d719be44637cf
Syn2ed <ayoub.laarobi@gmail.com>
2024-03-05 13:14:39 +0100

    Fix horizontal scroll

--- src/app/components/Home/Illustration.jsx
+++ src/app/components/Home/Illustration.jsx
@@ -3,7 +3,8 @@ import { Image } from "@nextui-org/react"
 
 export default function Illustration() {
   return (
-    <section className="relative max-w-screen-xl mx-auto md:px-8 flex flex-col justify-center items-center">
+    // Ajout de `overflow-x-hidden` pour éviter le défilement horizontal et `max-w-full` pour limiter la largeur de la section
+    <section className="relative max-w-full overflow-x-hidden mx-auto md:px-8 flex flex-col justify-center items-center">
         <Image isBlurred src="/images/home_illustration.png" width={500} height={500} />
     </section>
   );
